2eeacb70542f6fd49103211d3e7cbba450c7e211
Dominic Tremblay <dominictremblay24@gmail.com>
2024-10-30 17:23:04 -0400

    demo reset

--- App.js
+++ App.js
@@ -1,24 +1,15 @@
 import { useEffect, useState } from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
-import FilmVue from './fonctionalites/films/vues/Film.vue'
-import UseFilms from './hooks/useFilms'
-import collectionFilms from './data/films'
-import Exemple from './fonctionalites/exemple/Exemple'
-import Exemple1 from './fonctionalites/exemple/Exemple1'
+
 
 export default function App() {
 
 
-  const [state, dispatch] = UseFilms({
-    films: collectionFilms,
-    loading: true,
-  })
-
   return (
     <SafeAreaView style={styles.container}>
-      <FilmVue films={state.films} loading={state.loading} />
       {/* <Exemple1 /> */}
+      <Text>Demo useEffect</Text>
       <StatusBar style="auto" />
     </SafeAreaView>
   )
